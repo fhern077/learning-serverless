56ab6f41fbb2c150e44c70ed4d6fd456fd79f691
fhern077 <fhern077@fiu.edu>
2019-08-10 16:49:21 -0400

    updated with hello world v1

---
+++ serverless-scratch-helloworld/README.md
@@ -0,0 +1,7 @@
+## Hello World Function
+
+This function takes the event from the event.json and passes it to the handler function that is exported from ```helloworld.ts```.
+
+1. build the function with ```yarn build``` this exports the js to the dist folder
+2. execute the local invocation of the function with ```yarn invoke```
+3.
---
+++ serverless-scratch-helloworld/src/helloworld.ts
@@ -0,0 +1,15 @@
+/**
+ * Simple lambda handler that will return hello world
+ *
+ * @param  {any} _event
+ * The incoming message body to the the lambda handler this wil vary by trigger i.e dynamo, http_event, cloud_watch etc
+ * @param  {any} _context
+ * this contains metadata about the context in * which the function was invoked i.e environment arns etc.
+ * @param  {any} done
+ * the resolution of the lambda's invocation
+ *
+ *
+ */
+export const handler: any = async (_event: any, _context: any, done: any) => {
+  done(null, `👋  🌏`);
+};
